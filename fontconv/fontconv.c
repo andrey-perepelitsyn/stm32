1c684cc7ba7900e3a1de489ca13e115249d47f4a
Andrey Perepelitsyn <andrey.perepelitsyn@gmail.com>
2015-11-12 02:21:56 +0300

    reading BDF done

--- fontconv/fontconv.c
+++ fontconv/fontconv.c
@@ -4,7 +4,7 @@
  *  Created on: Nov 10, 2015
  *      Author: Andrey Perepelitsyn
  *
- *  tool converting *.bin font format produced by "Fony" to lcd_display font
+ *  tool for converting BDF font format produced by "Fony" to lcd_display font
  */
 
 #include <ctype.h>
@@ -12,8 +12,63 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdint.h>
 
-#define MAX_FONT_SIZE 20
+#define MAX_FONT_W 32
+#define MAX_FONT_H 64
+#define FONT_CHARS 224
+
+typedef struct {
+	int index, height, width;
+	char bits[MAX_FONT_H][MAX_FONT_W];
+} char_data_t;
+
+int bdf_read_char(FILE *f, char_data_t *c)
+{
+	int i, h, w, j, k, code;
+	char buf[20];
+	uint32_t line;
+
+	while((code = fscanf(f, "STARTCHAR %d", &i)) != 1)
+	{
+		if(code == EOF)
+			return -1;
+		fgetc(f);
+	}
+	while((code = fscanf(f, "BBX %d %d %d %d", &w, &h, &j, &k)) != 4)
+	{
+		if(code == EOF)
+			return -1;
+		fgetc(f);
+	}
+	do {
+		while((code = fscanf(f, "%19s", buf)) != 1)
+			if(code == EOF)
+				return -1;
+	} while(strcmp(buf, "BITMAP") != 0);
+	if(w > MAX_FONT_W || h > MAX_FONT_H)
+		return -1;
+	c->height = h;
+	c->width = w;
+	c->index = i;
+	if(h && w)
+		for(i = 0; i < h; i++)
+		{
+			fscanf(f, "%8s", buf);
+			for(j = 0; j < strlen(buf); j++)
+			{
+				int quad = buf[j];
+				if(quad >= 'A')
+					quad = quad - 'A' + 10;
+				else
+					quad -= '0';
+				for(k = 0; k < 4; k++)
+					c->bits[i][j*4 + k] = (quad >> (3 - k)) & 1;
+			}
+		}
+
+	return 0;
+}
 
 void die(const char *reason, const char *arg)
 {
@@ -25,9 +80,10 @@ void die(const char *reason, const char *arg)
 
 int main(int argc, char *argv[])
 {
-	int c, fontSize = 0;
+	int i, j, k, c, fontSize = 0;
 	FILE *src, *dst;
 	char *srcFileName = NULL, *dstFileName = NULL;
+	char_data_t char_data[FONT_CHARS];
 
 	opterr = 0;
 
@@ -51,7 +107,8 @@ int main(int argc, char *argv[])
 			}
 			break;
 		case 'h':
-			fputs("\nUsage:\n\tfontconv -s <size> [-i <source>] [-o <destination>]\n\n", stderr);
+			fputs(	"Tool for converting BDF font format to lcd_display font\n"
+					"\nUsage:\n\tfontconv -s <size> [-i <source>] [-o <destination>]\n\n", stderr);
 			return 1;
 		case 's':
 			fontSize = atoi(optarg);
@@ -67,31 +124,41 @@ int main(int argc, char *argv[])
 		}
 
 	fprintf(stderr, "source: '%s', destination: '%s'\n", srcFileName, dstFileName);
-	if(fontSize == 0) {
-		fputs("font size is not defined!\n", stderr);
-		return 1;
-	}
+//	if(fontSize == 0) {
+//		fputs("font size is not defined!\n", stderr);
+//		return 1;
+//	}
 	if(srcFileName == NULL)
 		src = stdin;
 	if(dstFileName == NULL)
 		dst = stdout;
 
-	c = ' ';
+	memset(char_data, 0, sizeof(char_data));
 	while(!feof(src))
 	{
-		char bitmap[MAX_FONT_SIZE];
-		int i, j;
-		if(fread(bitmap, fontSize, 1, src) != 1)
+		char_data_t ch;
+		if(bdf_read_char(src, &ch))
 			break;
-		fprintf(dst, "# '%c' 0x%02X\n", c, c);
-		for(i = 0; i < fontSize; i++)
-		{
-			for(j = 7; j >= 0; j--)
-				fputc(((bitmap[i] >> j) & 1) + '0', dst);
-			fputs("b\n", dst);
+		if(ch.index < 32 || ch.index > 255) {
+			fprintf(stderr, "char #%02x out of range, skipping.\n", ch.index);
+			continue;
 		}
-		c++;
+		fprintf(stderr, "char #%02x read ok.\n", ch.index);
+		char_data[ch.index - 32] = ch;
 	}
 
+	for(i = 0; i < FONT_CHARS; i++)
+	{
+		if(char_data[i].index) {
+			fprintf(stderr, "i: 0x%02x '%c', h: %d, w: %d\n",
+					char_data[i].index, char_data[i].index, char_data[i].height, char_data[i].width);
+			for(j = 0; j < char_data[i].height; j++)
+			{
+				for(k = 0; k < char_data[i].width; k++)
+					fputc(char_data[i].bits[j][k] ? '#' : '.', stderr);
+				fputc('\n', stderr);
+			}
+		}
+	}
 	return 0;
 }
